bbf06b7d31a0248ccae6431e9532e65d7a421772
Mehdi-SK <zoubirimehdi64@gmail.com>
2026-05-25 13:54:14 +0200

    feat(pull-request): add creditAuthors and creditReviewers functions
    - Implemented creditAuthors function to process pull request authors.
    - Implemented creditReviewers function to process pull request reviewers.
    - Both functions currently throw an error indicating they are not implemented.

---
+++ src/strategies/pull-request/shared/author-credits.ts
@@ -0,0 +1,8 @@
+import { ContributionPayload } from "../../../types/contribution-payload.type.js";
+import { TPullRequestPayload } from "../../../types/payloads/payload.types.js";
+
+export function creditAuthors(
+  payload: TPullRequestPayload
+): ContributionPayload[] {
+  throw new Error('Method not implemented.')
+}
---
+++ src/strategies/pull-request/shared/reviwer-credits.ts
@@ -0,0 +1,8 @@
+import { ContributionPayload } from "../../../types/contribution-payload.type.js";
+import { TPullRequestPayload } from "../../../types/payloads/payload.types.js";
+
+export function creditReviewers(
+  payload: TPullRequestPayload
+): ContributionPayload[] {
+  throw new Error('Method not implemented.')
+}
